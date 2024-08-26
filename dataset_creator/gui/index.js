a85eaebbc1fbd740d9e5543e334b503ad7042814
Santiago Fiorino <sfiorino@dc.uba.ar>
2024-08-26 15:00:47 -0300

    gui: effects category and GET .json requests only once

--- dataset_creator/gui/index.js
+++ dataset_creator/gui/index.js
@@ -1,8 +1,46 @@
 const AUDIOS_PER_PAGE = 4
 let audiosToShow = []
+let idToData = {}
 let page = 0
-let instruments = {}
-let genres = {}
+
+let genreToIds = {}
+let instrumentToIds = {}
+let effectToId = {}
+
+document.getElementById('categoriesList').insertAdjacentHTML('beforeend',sideBarCategoryElement('Genres'))
+document.getElementById('categoriesList').insertAdjacentHTML('beforeend', sideBarCategoryElement('Instruments'))
+document.getElementById('categoriesList').insertAdjacentHTML('beforeend',sideBarCategoryElement('Effects'))
+
+function sideBarCategoryElement(categoryName) {
+    return `
+        <div x-data="{ isActive: false, open: false }">
+            <a
+                @click="$event.preventDefault(); open = !open"
+                class="flex items-center p-2 text-zinc-50 font-bold transition-colors rounded-md hover:bg-zinc-600"
+                :class="{'bg-zinc-600': isActive || open}"
+                role="button"
+                aria-haspopup="true"
+                :aria-expanded="(open || isActive) ? 'true' : 'false'"
+            >
+            <span class="ml-2 text-xl"> ${categoryName} </span>
+            <span aria-hidden="true" class="ml-auto">
+                <svg
+                class="w-4 h-4 transition-transform transform"
+                :class="{ 'rotate-180': open }"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                >
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                </svg>
+            </span>
+            </a>
+            <div id="${categoryName}List" x-show="open" class="mt-2 space-y-2 px-7" role="menu">
+            </div>
+        </div>
+    `
+}
 
 fetch('/filenames')
     .then(response => response.json())
@@ -16,107 +54,113 @@ fetch('/filenames')
                     data.id = id
                     addGenre(data)
                     addInstrument(data)
+                    addEffect(data)
+                    idToData[id] = data
                 })
             })
     })
 
 function addGenre(data) {
-    if (data.genre in genres) {
-        genres[data.genre].push(data.id)
+    if (data.genre in genreToIds) {
+        genreToIds[data.genre].push(data.id)
         let genreBadge = document.getElementById(data.genre + 'Badge')
         genreBadge.innerText = parseInt(genreBadge.innerText) + 1
     } else {
-        genres[data.genre] = [data.id]
-        let genresList = document.getElementById('genresList')
-        const newGenreButton = `
-            <a
-                onclick="showGenre('${data.genre}')"
-                role="menuitem"
-                class="block p-2 text-md font-bold text-zinc-50 transition-colors duration-200 rounded-md hover:text-violet-600 select-none"
-            >
-                ${data.genre}
-                <span id="${data.genre}Badge" class="bg-violet-900 text-zinc-50 text-xs font-bold me-2 ml-2 px-2.5 py-0.5 rounded-xl select-none">
-                    1
-                </span>
-            </a>
-        `
-        genresList.insertAdjacentHTML('beforeend', newGenreButton);
+        genreToIds[data.genre] = [data.id]
+        let genresList = document.getElementById('GenresList')
+        genresList.insertAdjacentHTML('beforeend', sideBarItemElement("showGenre", data.genre));
     }
 }
 
 function addInstrument(data) {
-    if (data.instrument in instruments) {
-        instruments[data.instrument].push(data.id)
+    if (data.instrument in instrumentToIds) {
+        instrumentToIds[data.instrument].push(data.id)
         let instrumentBadge = document.getElementById(data.instrument + 'Badge')
         instrumentBadge.innerText = parseInt(instrumentBadge.innerText) + 1
     } else {
-        instruments[data.instrument] = [data.id]
-        let instrumentsList = document.getElementById('instrumentsList')
-        const newInstrumentButton = `
-            <a
-                onclick="showInstrument('${data.instrument}')"
-                role="menuitem"
-                class="block p-2 text-md font-bold text-zinc-50 transition-colors duration-200 rounded-md hover:text-violet-600 select-none hover:cursor-pointer"
-            >
-                ${data.instrument}
-                <span id="${data.instrument}Badge" class="bg-violet-900 text-zinc-50 text-xs font-bold me-2 ml-2 px-2.5 py-0.5 rounded-xl select-none">
-                    1
-                </span>
-            </a>
-        `
-        instrumentsList.insertAdjacentHTML('beforeend', newInstrumentButton);
+        instrumentToIds[data.instrument] = [data.id]
+        let instrumentsList = document.getElementById('InstrumentsList')
+        instrumentsList.insertAdjacentHTML('beforeend', sideBarItemElement('showInstrument', data.instrument));
     }
 }
 
+function addEffect(data) {
+    const effects = 'pedals' in data ? data.pedals : [];
+    effects.forEach(effect => {
+        if (effect.name in effectToId) {
+            effectToId[effect.name].push(data.id)
+            let effectBadge = document.getElementById(effect.name + 'Badge')
+            effectBadge.innerText = parseInt(effectBadge.innerText) + 1
+        } else {
+            effectToId[effect.name] = [data.id]
+            let effectsList = document.getElementById('EffectsList')
+            effectsList.insertAdjacentHTML('beforeend', sideBarItemElement('showEffect', effect.name));
+        }
+    })
+}
+
+function sideBarItemElement(functionName, buttonName) {
+    return `
+        <a
+            onclick="${functionName}('${buttonName}')"
+            role="menuitem"
+            class="block p-2 text-md font-bold text-zinc-50 transition-colors duration-200 rounded-md hover:text-violet-600 select-none hover:cursor-pointer"
+        >
+            ${buttonName}
+            <span id="${buttonName}Badge" class="bg-violet-900 text-zinc-50 text-xs font-bold me-2 ml-2 px-2.5 py-0.5 rounded-xl select-none">
+                1
+            </span>
+        </a>
+        `
+}
+
 function showGenre(genre) {
-    page = 0
-    document.getElementById('pageButtons').className = ""
-    document.getElementById('mainTitle').innerText = genre + " Songs"
-    
-    audiosToShow = genres[genre]
-    showAudios(genres[genre])
+    showNewList(genre + " Songs", genreToIds[genre])
 }
 
 function showInstrument(instrument) {
-    page = 0
-    document.getElementById('pageButtons').className = ""
-    document.getElementById('mainTitle').innerText = instrument + " Songs"
-    
-    audiosToShow = instruments[instrument]
-    showAudios(instruments[instrument])
+    showNewList("Songs played with " + instrument, instrumentToIds[instrument])
 }
 
-function showAudios(audioIdList) {
+function showEffect(effect) {
+    showNewList("Songs with " + effect, effectToId[effect])
+}
+
+function showNewList(newTitle, newList) {
+    page = 0
+    document.getElementById('prevNextButtons').className = ""
+    document.getElementById('mainTitle').innerText = newTitle
+    audiosToShow = newList
+    updateShowingAudios()
+}
+
+function updateShowingAudios(){
     const audiosOnDisplay = document.getElementById('audioList')
     audiosOnDisplay.innerHTML = ""
     const startIndex = page * AUDIOS_PER_PAGE;
-    const endIndex = Math.min((page + 1) * AUDIOS_PER_PAGE, audioIdList.length)
-    audioIdList.slice(startIndex, endIndex).forEach((audioId, index) => {
+    const endIndex = Math.min((page + 1) * AUDIOS_PER_PAGE, audiosToShow.length)
+    audiosToShow.slice(startIndex, endIndex).forEach((audioId, index) => {
         showAudio(audioId)
     })
 }
 
-function showAudio(id) {
-    fetch(`/renders/${id}.json`)
-        .then(response => response.json())
-        .then(data => {
-            data.id = id
-            const audioList = document.getElementById('audioList')
-            const newItemHTML = audioToListElement(data);
-            audioList.insertAdjacentHTML('beforeend', newItemHTML)
-        })
+function showAudio(audioId) {
+    const audioList = document.getElementById('audioList')
+    const newItemHTML = idToListElement(audioId);
+    audioList.insertAdjacentHTML('beforeend', newItemHTML)
 }
 
-function audioToListElement(data) {
+function idToListElement(audioId) {
+    const data = idToData[audioId]
     const instrumentBadge = `
     <span class="bg-violet-800 text-zinc-50 text-xs font-bold me-2 ml-2 px-2.5 py-0.5 rounded-xl select-none">
     ${data.instrument}
     </span>
     `
-    const pedals = 'pedals' in data ? data.pedals : [];
-    const pedalBadges = pedals.map(pedal => 
+    const effects = 'pedals' in data ? data.pedals : [];
+    const effectsBadges = effects.map(effect => 
         `<span class="bg-blue-800 text-zinc-50 text-xs font-bold me-2 px-2.5 py-0.5 rounded-xl select-none">
-            ${pedal.name}
+            ${effect.name}
         </span>`
     ).join('');
 
@@ -143,9 +187,9 @@ function audioToListElement(data) {
                     <p class="text-md font-black leading-6 text-zinc-50">
                         ${data.song}
                         ${instrumentBadge}
-                        ${pedalBadges}
+                        ${effectsBadges}
                     </p>
-                    <p class="mt-1 truncate text-xs font-semibold leading-5 text-zinc-600">${data.artist}</p>
+                    <p class="mt-1 truncate text-xs font-semibold leading-5 text-zinc-600">${data.artist} | ${data.specific_genre}</p>
                 </div>
             </div>
 
@@ -201,18 +245,20 @@ function deleteAudio(id){
             'Content-Type': 'application/json'
         }
     })
-    .then(response => console.log(response))
+    .then(
+        updateShowingAudios()
+    )
     .catch(error => console.error('Error:', error))
 }
 
 function previousPage() {
     if (page*AUDIOS_PER_PAGE - AUDIOS_PER_PAGE < 0) return
     page -= 1
-    showAudios(audiosToShow)
+    updateShowingAudios()
 }
 
 function nextPage() {
     if (page*AUDIOS_PER_PAGE + AUDIOS_PER_PAGE >= audiosToShow.length) return
     page += 1
-    showAudios(audiosToShow)
+    updateShowingAudios()
 }
